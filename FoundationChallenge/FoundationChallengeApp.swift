b785682fdb4b60b8fa95c9509c268a9d4e45d071
Lazizbek <mywave.17@gmail.com>
2024-12-05 16:36:59 +0100

    Initial Commit

---
+++ FoundationChallenge/FoundationChallengeApp.swift
@@ -0,0 +1,17 @@
+//
+//  FoundationChallengeApp.swift
+//  FoundationChallenge
+//
+//  Created by Lazizbek Fayzullayev on 05/12/24.
+//
+
+import SwiftUI
+
+@main
+struct FoundationChallengeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
