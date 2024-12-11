c00a32dedf15182e434ae447dbf0cb1bed2b2360
Lazizbek <mywave.17@gmail.com>
2024-12-11 13:48:53 +0100

    added day infos for calendar

--- FoundationChallenge/Views/CalendarView.swift
+++ FoundationChallenge/Views/CalendarView.swift
@@ -6,27 +6,40 @@
 //
 
 import SwiftUI
+import SwiftData
 
 struct CalendarView: View {
+    
     @State private var selectedDate = Date()
+    @Query(sort: \Reminder.date, order: .forward) private var reminders: [Reminder]
     
     var body: some View {
+        
         NavigationStack {
-            ScrollView {
-                VStack {
-                    DatePicker(
-                        "Choose a date",
-                        selection: $selectedDate,
-                        displayedComponents: [.date]
-                    )
-                    .datePickerStyle(GraphicalDatePickerStyle())
-                    .padding()
-                    
-                    Spacer()
-                }
-            }
+                    VStack {
+                        DatePicker(
+                            "Choose a date",
+                            selection: $selectedDate,
+                            displayedComponents: [.date]
+                        )
+                        .datePickerStyle(GraphicalDatePickerStyle())
+                        .padding()
+                        Spacer()
+                        
+                        List {
+                            ForEach(reminders) { reminder in
+                                if reminder.date == selectedDate {
+                                    ReminderRow(reminder: reminder)
+                                }
+    
+                            }
+                        }
+                    }
+                
+                
+                
+
             .background(Color(.systemGroupedBackground))
-            
             .navigationTitle("Calendar")
         }
     }
